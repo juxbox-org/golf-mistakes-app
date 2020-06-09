d98853381fa06db8276cf8b8ca6b6b55f9485929
James Ong <j.simon.ong@gmail.com>
2020-06-08 18:57:40 -0600

    - Added vuex-persist to save/restore to local storage

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,11 +1,15 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
+import VuexPersistence from 'vuex-persist';
 // import hasKey from '@/utils/objects';
 import mistakeDefs from './mistake-defs/mistake-defs';
 import rounds from './rounds/rounds';
 import currentRound from './current-round/current-round';
+import { RootState } from './rootTypes.d';
 
 Vue.use(Vuex);
+
+const vuexLocal = new VuexPersistence<RootState>();
 
 export default new Vuex.Store({
   state: {
@@ -26,4 +30,5 @@ export default new Vuex.Store({
     rounds,
     currentRound,
   },
+  plugins: [vuexLocal.plugin],
 });
